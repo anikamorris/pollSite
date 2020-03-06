65c0cae3443dda3f3777feb718df45871faa7317
anikamorris <anikamorris11@gmail.com>
2020-03-05 20:18:52 -0800

    added DRF and deployed to heroku

---
+++ api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework.serializers import ModelSerializer
+
+from polls.models import Question, Choice
+
+class QuestionSerializer(ModelSerializer):
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+
+class ChoiceSerializer(ModelSerializer):
+    class Meta:
+        model = Question
+        fields = '__all__'
+        
